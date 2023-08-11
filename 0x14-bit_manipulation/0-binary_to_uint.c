287b6c5f1f8f7aac18af53251051995257817635
collinswod <wodopeyc@gmail.com>
2023-08-11 01:07:26 +0000

    Bit Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,29 +9,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int Unsigned_int;
-	int len, base_two;
+unsigned int Unsigned_int;
+int len, base_two;
 
-	if (!b)
-		return (0);
-
-	Unsigned_int = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			Unsigned_int += base_two;
-		}
-	}
-
-	return (Unsigned_int);
+if (!b)
+return (0);
+Unsigned_int = 0;
+for (len = 0; b[len] != '\0'; len++);
+for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+{
+if (b[len] != '0' && b[len] != '1')
+{
+return (0);
+}
+if (b[len] & 1)
+{
+Unsigned_int += base_two;
+}
+}
+return (Unsigned_int);
 }
